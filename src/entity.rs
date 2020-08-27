3c8370df8159d0f654cddfaf9acfa449350b4b0a
Starz0r <starz0r@starz0r.com>
2020-08-26 22:11:06 -0500

    entity: include more information like rotation and axis flipping

--- src/entity.rs
+++ src/entity.rs
@@ -3,12 +3,25 @@ use std::io::{Result, Write};
 
 use byteorder::{LittleEndian, WriteBytesExt};
 
-pub fn create(mut f: &File, ent: i32, x: i32, y: i32, w: u32, h: u32) -> Result<()> {
+pub fn create(
+    mut f: &File,
+    ent: i32,
+    x: i32,
+    y: i32,
+    w: u32,
+    h: u32,
+    rot: i16,
+    fx: bool,
+    fy: bool,
+) -> Result<()> {
     f.write_u8(0x3)?; // entity create instruction
-    f.write_i32::<LittleEndian>(ent)?; // entity operand
-    f.write_i32::<LittleEndian>(x)?; // x operand
-    f.write_i32::<LittleEndian>(y)?; // y operand
-    f.write_u32::<LittleEndian>(w)?; // width operand
-    f.write_u32::<LittleEndian>(h)?; // height operand
+    f.write_i32::<LittleEndian>(ent)?;   // entity operand
+    f.write_i32::<LittleEndian>(x)?;     // x operand
+    f.write_i32::<LittleEndian>(y)?;     // y operand
+    f.write_u32::<LittleEndian>(w)?;     // width operand
+    f.write_u32::<LittleEndian>(h)?;     // height operand
+    f.write_i16::<LittleEndian>(rot)?;   // rotation operand
+    f.write_u8(fx as u8)?;               // flipped x operand
+    f.write_u8(fy as u8)?;               // flipped y operand
     Ok(())
 }
