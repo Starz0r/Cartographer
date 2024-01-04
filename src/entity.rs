4828342f35cf4277919b292a032bb2b7742f20db
Starz0r <starz0r@starz0r.com>
2024-01-04 00:02:10 -0600

    entity: utility code for writing entities to files

--- src/entity.rs
+++ src/entity.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Result, Write};
+use std::io::Result;
 
 use byteorder::{LittleEndian, WriteBytesExt};
 
@@ -15,13 +15,13 @@ pub fn create(
     fy: bool,
 ) -> Result<()> {
     f.write_u8(0x3)?; // entity create instruction
-    f.write_i32::<LittleEndian>(ent)?;   // entity operand
-    f.write_i32::<LittleEndian>(x)?;     // x operand
-    f.write_i32::<LittleEndian>(y)?;     // y operand
-    f.write_u32::<LittleEndian>(w)?;     // width operand
-    f.write_u32::<LittleEndian>(h)?;     // height operand
-    f.write_i16::<LittleEndian>(rot)?;   // rotation operand
-    f.write_u8(fx as u8)?;               // flipped x operand
-    f.write_u8(fy as u8)?;               // flipped y operand
+    f.write_i32::<LittleEndian>(ent)?; // entity operand
+    f.write_i32::<LittleEndian>(x)?; // x operand
+    f.write_i32::<LittleEndian>(y)?; // y operand
+    f.write_u32::<LittleEndian>(w)?; // width operand
+    f.write_u32::<LittleEndian>(h)?; // height operand
+    f.write_i16::<LittleEndian>(rot)?; // rotation operand
+    f.write_u8(fx as u8)?; // flipped x operand
+    f.write_u8(fy as u8)?; // flipped y operand
     Ok(())
 }
